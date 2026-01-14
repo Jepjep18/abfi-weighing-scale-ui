08edd1cd50bfb7a3f5524ac35f6b12d074ab7e72
James Jecemeco A. Tabilog <jecemecojamestabilog@gmail.com>
2026-01-14 09:07:43 +0800

    feat: add product classification models

--- src/app/models/product-classification/product-classification.model.ts
+++ src/app/models/product-classification/product-classification.model.ts
@@ -6,4 +6,28 @@ export interface ProductClassification {
   noOfHeadsPerGalantina: number;
   cratesWeight: string;
   isActive: boolean;
+  createdAt: string;
+  lastUpdatedAt?: string;
+}
+
+export interface CreateProductClassicationDto {
+productCode: string;
+individualWeightRange: string;
+totalWeightRangePerCrate: string;
+noOfHeadsPerGalantina?: number;
+cratesWeight: string;
+isActive?: boolean;
+}
+
+export interface UpdateProductClassificationDto extends CreateProductClassicationDto{
+
+}
+
+export interface ProductClassificationListDto {
+    id: number;
+    productCode?: string;
+}
+
+export interface ProductClassificationFileUploadDto {
+    file: File;
 }
